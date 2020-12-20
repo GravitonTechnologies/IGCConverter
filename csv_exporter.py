b31212b890369513bcca37b519e66830ac1d5e02
Nicolas Ballas <ni2k150@gmail.com>
2020-12-19 22:59:58 -0500

    Basic CSV exporter

--- csv_exporter.py
+++ csv_exporter.py
@@ -7,31 +7,34 @@ class CSVFlightInfoExporter(FlightInfoExporter):
     def __init__(self):
         self.flight_info: Optional[FlightInfo] = None
         self.titles = ['UTC Time', 'Longitude', 'Latitude', 'GPS Alt', 'Fix Validity', 'Pressure Altitude']
+        self.writer = None
+
+    def _export_flight_data(self):
+        # Add titles to table
+        for info_data in self.flight_info.timed_flight_data:
+            if info_data.additional_data is not None:
+                for title in info_data.additional_data.keys():
+                    if title not in self.titles:
+                        self.titles.append(title)
+        self.writer.writerow(self.titles)
+
+        # TODO: Make sure value is always aligned with table title
+        # Add values to table
+        for info_data in self.flight_info.timed_flight_data:
+            # standard values
+            values = [info_data.utc_time, info_data.longitude, info_data.latitude, info_data.gps_altitude,
+                      info_data.fix_validity, info_data.pressure_altitude]
+
+            # optional values
+            if info_data.additional_data is not None:
+                for indices in info_data.additional_data.values():
+                    value = info_data.raw_data[indices[0]:indices[1]]
+                    values.append(value)
+
+            self.writer.writerow(values)
 
     def export(self, flight_info: FlightInfo, destination_path: str):
         self.flight_info = flight_info
         with open(destination_path, 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-
-            # Add titles to table
-            for info_data in flight_info.timed_flight_data:
-                if info_data.additional_data is not None:
-                    for title in info_data.additional_data.keys():
-                        if title not in self.titles:
-                            self.titles.append(title)
-            writer.writerow(self.titles)
-
-            # TODO: Make sure value is always aligned with table title
-            # Add values to table
-            for info_data in flight_info.timed_flight_data:
-                # standard values
-                values = [info_data.utc_time, info_data.longitude, info_data.latitude, info_data.gps_altitude,
-                          info_data.fix_validity, info_data.pressure_altitude]
-
-                # optional values
-                if info_data.additional_data is not None:
-                    for indices in info_data.additional_data.values():
-                        value = info_data.raw_data[indices[0]:indices[1]]
-                        values.append(value)
-
-                writer.writerow(values)
+            self.writer = csv.writer(csvfile)
+            self._export_flight_data()
