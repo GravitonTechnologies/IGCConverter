190392cc3b8f335306d29172021533311453f34b
nicolaos.ballas.1@ulaval.ca <nicolaos.ballas.1@ulaval.ca>
2020-12-22 07:18:53 -0500

    Added help

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from igc_converter import convert_igc
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--format", default='csv', type=str)
-    parser.add_argument("input", type=str)
+    parser.add_argument("--format", default='csv', type=str, help='output format')
+    parser.add_argument("input", type=str, help='IGC source')
     parser.add_argument('--gui', action='store_true', help='start in GUI mode')
     args = parser.parse_args()
 
