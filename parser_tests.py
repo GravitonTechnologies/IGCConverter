f3600adefef0e5c6fe8d028e1904446fb1a06fa6
nicolaos.ballas.1@ulaval.ca <nicolaos.ballas.1@ulaval.ca>
2020-12-22 07:26:32 -0500

    Added extension header test

--- parser_tests.py
+++ parser_tests.py
@@ -17,6 +17,14 @@ class MyTestCase(unittest.TestCase):
         parser._parse_flight_recorder_info('AFLA9WL')
         self.assertEqual(self.flight_info.flight_recorder_info.flight_recorder_manufacturer_code, 'FLA')
 
+    def test_extension_header(self):
+        parser = IGCParser()
+        parser.flight_info = self.flight_info
+        parser._parse_extension_header('I023638FXA3940SIU')
+        self.assertEqual(self.flight_info.extension_header.num_extensions, 2)
+        self.assertEqual(self.flight_info.extension_header.flight_data['FXA'], (36, 38))
+        self.assertEqual(self.flight_info.extension_header.flight_data['SIU'], (39, 40))
+
 
 if __name__ == '__main__':
     unittest.main()
