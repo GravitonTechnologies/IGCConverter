e4905e1bbcf912b5c5046f484210bca73679e602
Nicolas Ballas <ni2k150@gmail.com>
2020-12-24 14:16:57 -0500

    added test_complex_extended_timed_flight_data

--- parser_tests.py
+++ parser_tests.py
@@ -21,8 +21,8 @@ class MyTestCase(unittest.TestCase):
     def test_extension_header(self):
         self.parser._parse_extension_header('I023638FXA3940SIU')
         self.assertEqual(self.flight_info.extension_header.num_extensions, 2)
-        self.assertEqual(self.flight_info.extension_header.flight_data['FXA'], (36, 38))
-        self.assertEqual(self.flight_info.extension_header.flight_data['SIU'], (39, 40))
+        self.assertEqual(self.flight_info.extension_header.extended_data_indices['FXA'], (36, 38))
+        self.assertEqual(self.flight_info.extension_header.extended_data_indices['SIU'], (39, 40))
 
     def test_basic_timed_flight_data(self):
         # No additional info was found
@@ -30,7 +30,7 @@ class MyTestCase(unittest.TestCase):
         timed_data_list = self.flight_info.timed_flight_data
         self.assertEqual(len(timed_data_list), 1)
         timed_data = timed_data_list[0]
-        self.assertIsNone(timed_data.extensions)
+        self.assertIsNone(timed_data.extension_values)
         self.assertEqual(timed_data.utc_time, '151109')
         self.assertEqual(timed_data.latitude, '4538002N')
         self.assertEqual(timed_data.longitude, '07249279W')
@@ -43,10 +43,25 @@ class MyTestCase(unittest.TestCase):
         self.parser._parse_timed_flight_data('B1511094538002N07249279WA-00940004000109')
 
         self.assertEqual(len(self.flight_info.timed_flight_data), 1)
-        self.assertIsNotNone(self.flight_info.timed_flight_data[0].extension_values)
 
         self.assertEqual(self.flight_info.timed_flight_data[0].extension_values['FXA'], '001')
         self.assertEqual(self.flight_info.timed_flight_data[0].extension_values['SIU'], '09')
+
+    def test_complex_extended_timed_flight_data(self):
+        self.parser._parse_extension_header('I083638FXA3941ENL4246TAS4751GSP5254TRT5559VAT6063OAT6467ACZ')
+        self.parser._parse_timed_flight_data('B1723034538174N07249500WA000080011200701212277143713160034802090098')
+
+        self.assertEqual(len(self.flight_info.timed_flight_data), 1)
+
+        self.assertEqual(self.flight_info.extension_header.num_extensions, 8)
+        self.assertEqual(self.flight_info.timed_flight_data[0].extension_values['FXA'], '007')
+        self.assertEqual(self.flight_info.timed_flight_data[0].extension_values['ENL'], '012')
+        self.assertEqual(self.flight_info.timed_flight_data[0].extension_values['TAS'], '12277')
+        self.assertEqual(self.flight_info.timed_flight_data[0].extension_values['GSP'], '14371')
+        self.assertEqual(self.flight_info.timed_flight_data[0].extension_values['TRT'], '316')
+        self.assertEqual(self.flight_info.timed_flight_data[0].extension_values['VAT'], '00348')
+        self.assertEqual(self.flight_info.timed_flight_data[0].extension_values['OAT'], '0209')
+        self.assertEqual(self.flight_info.timed_flight_data[0].extension_values['ACZ'], '0098')
 
 
 if __name__ == '__main__':
