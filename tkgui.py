f6e62affd2335da66530dd2bba532dc5853cd21f
nicolaos.ballas.1@ulaval.ca <nicolaos.ballas.1@ulaval.ca>
2021-01-15 18:40:39 -0500

    Changed Tk GUI size

--- tkgui.py
+++ tkgui.py
@@ -12,7 +12,7 @@ class IGCTKConverterGUI(ConversionProgressObserver, IGCConverterExceptionObserve
 
     def __init__(self):
         self.app = tk.Tk()
-        self.app.geometry('500x50')
+        self.app.geometry('250x60')
         self.app.winfo_toplevel().title("IGC Converter")
         self.selected_igc_path = None
         self._num_converted_files = 0
